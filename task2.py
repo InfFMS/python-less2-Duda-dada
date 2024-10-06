2972b4ff3d83d2e9f8e9c22cad262c80033b6db1
Ira <dudairocka@gmail.com>
2024-10-06 17:56:20 +0300

    Решила 2 задание

---
+++ task2.py
@@ -0,0 +1,11 @@
+n = int(input())
+if n in [1, 2, 12]:
+    print('Зима')
+elif n in [3, 4, 5]:
+    print('Весна')
+elif n in [6, 7, 8]:
+    print('Лето')
+elif n in [9, 10, 11]:
+    print('Осень')
+else:
+    print('Неверный номер месяца')
